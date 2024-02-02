964710933afedfa1f80b499d992aeda28b57b96d
DannySanchez03 <dannyproa303@gmail.com>
2024-02-02 00:00:36 -0600

    Finished typing all the code out for the index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,69 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter your project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a brief description of your project: ',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide any installations needed to run the project if needed: ',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter the usage of this project: ',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter the names of the contributers: ',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter any tests that were used for this project: ',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email: ',
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Enter your github usaername: ',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        const filename = 'README.md';
+        const readmeContent = generateMarkdown(data);
+        writeToFile(filename, readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
